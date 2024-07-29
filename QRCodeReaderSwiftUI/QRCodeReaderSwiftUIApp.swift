5e901c292367eafac37bbea2e506865e3bc7d9e3
Jeongseok Kang <ubermensch1218@gmail.com>
2024-07-30 00:55:39 +0900

    Initial Commit

---
+++ QRCodeReaderSwiftUI/QRCodeReaderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeReaderSwiftUIApp.swift
+//  QRCodeReaderSwiftUI
+//
+//  Created by Jeongseok Kang on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeReaderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QRCodeReaderSwiftUIUITests/QRCodeReaderSwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  QRCodeReaderSwiftUIUITestsLaunchTests.swift
+//  QRCodeReaderSwiftUIUITests
+//
+//  Created by Jeongseok Kang on 7/30/24.
+//
+
+import XCTest
+
+final class QRCodeReaderSwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
